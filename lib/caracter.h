83a1e4253cb11e377df9f64dca934e3923bccb39
Luis Escobedo <luis.escobedo@uinenlinea.mx>
2021-08-19 00:36:18 -0500

    Se creo la libreria para los caracteres de filas y columnas

---
+++ lib/caracter.h
@@ -0,0 +1,23 @@
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int Wsize ()
+{
+
+   
+
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+    // printf ("lines %d\n", w.ws_row);
+    // printf ("columns %d\n", w.ws_col);
+
+    for (int i = 0; i < w.ws_col; i++)
+    {
+        printf("=");
+    }
+    
+
+    return 0;  // make sure your main returns int
+}
--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 #include<string.h>
-
+#include"lib/caracter.h"
 #define USER "admin"
 #define PASSWORD "123"
 int main(){
-
     char user [80];
     char clave [80];
+
+    Wsize();
+
     printf("\t\t Inicio de Sesion\n");
-    
-    printf("\t\t Usuario\n");
+    Wsize();
+
+    printf("\n\t\t Usuario\n");
     printf("\t\t");
     scanf("%s",user);
 
